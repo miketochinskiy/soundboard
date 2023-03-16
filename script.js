d89dbe726541bcb8783aa08fe4f5512e9e8128cf
miketochinskiy <120083573+miketochinskiy@users.noreply.github.com>
2023-03-16 22:30:34 +0000

    Added More Sounds

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
+
+const body = document.querySelector('body');
+const birdsButton = document.querySelector('#birds');
+const rainButton = document.querySelector('#rain');
+const fireButton = document.querySelector('#fire');
+
+birdsButton.addEventListener('click', () => {
+  body.classList.remove('rain', 'fire');
+  body.classList.add('birds');
+});
+
+rainButton.addEventListener('click', () => {
+  body.classList.remove('birds', 'fire');
+  body.classList.add('rain');
+});
+
+fireButton.addEventListener('click', () => {
+  body.classList.remove('birds', 'rain');
+  body.classList.add('fire');
+});
+
+
+
 const playSound = e => {
       let keyCode;
       if (e.type === 'keydown') {
+        
         keyCode = e.keyCode;
+        body.classList.add('birds');
+
       } else {
         keyCode = e.target.getAttribute('data-key') || e.target.parentNode.getAttribute('data-key');
       }
@@ -34,23 +60,3 @@ const playSound = e => {
 
 
 
-
-    const body = document.querySelector('body');
-    const birdsButton = document.querySelector('#birds');
-    const rainButton = document.querySelector('#rain');
-    const fireButton = document.querySelector('#fire');
-
-    birdsButton.addEventListener('click', () => {
-      body.classList.remove('rain', 'fire');
-      body.classList.add('birds');
-    });
-
-    rainButton.addEventListener('click', () => {
-      body.classList.remove('birds', 'fire');
-      body.classList.add('rain');
-    });
-
-    fireButton.addEventListener('click', () => {
-      body.classList.remove('birds', 'rain');
-      body.classList.add('fire');
-    });
